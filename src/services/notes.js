e67e1ff075404dea58995ed5fc67aec5b14d8753
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-03-25 13:45:18 +0100

    Update backend url

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/notes";
+const baseUrl = "https://rg-fso-notes.herokuapp.com/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);

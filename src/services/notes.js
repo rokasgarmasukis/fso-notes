d97a696cf5efb31049a19f6275657bb31b80fb02
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-03-25 14:54:54 +0100

    fix

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,19 +1,19 @@
 import axios from "axios";
-const baseUrl = "https://rg-fso-notes.herokuapp.com/api/notes";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  return request.then(response => response.data)
+  return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
   const request = axios.post(baseUrl, newObject);
-  return request.then(response => response.data)
+  return request.then((response) => response.data);
 };
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then(response => response.data)
+  return request.then((response) => response.data);
 };
 
 export default {
